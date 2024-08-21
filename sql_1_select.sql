df8b71dfbe1392dd94318e656da27429dd4e9cd2
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-08-21 10:03:04 +0200

    Modified

--- sql_1_select.sql
+++ sql_1_select.sql
@@ -16,56 +16,60 @@
 # 1: Get a query to return "Hello World", 123
 # (Hint: 1 row, 2 columns)
 SELECT "hello world" as message , 123 as id;
-#
+#countrylanguage
 # 2: Get everything from the city table
 # (Hint: Many many rows)
-#
+SELECT * from `city`;
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
-#
+SELECT * from city WHERE District = "aceh";
 #
 # 4: Get only the name of the cities where the countrycode is "bfa"
-#
+SELECT Name  from city WHERE countryCode = "bfa";
 #
 # 5: Get both the name and district of the cities where the countrycode is "tto"
-#
+SELECT `Name`, `District` FROM city WHERE `countryCode` = "tto";
 #
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
-#
+SELECT `Name` as nm, `District` as dist from `city` WHERE `countryCode` = "arm";
 #
 # 7: Get the cities with a name that starts with "bor"
-#
+SELECT `name` FROM `city`where `name` like "bor%";
 #
 # 8: Get the cities with a name that contains the string "orto"
-#
+SELECT `name` from `city` WHERE `name` like "%orto%";
 #
 # 9: Get the cities that has a population below 1000
-#
+SELECT `name` from `city` where `population` <=1000;
 #
 # 10: Get the unique countrycodes from the cities that has a population below 1000
-#
+SELECT DISTINCT `countryCode` from `city` where `population` <=1000; 
 #
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
-#
+SELECT `names` from `city` where `countryCode` = `UKR` and `population` >=1000000;
 #
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
-#
+SELECT * from `city` where `population` <200 and `population` >=9500000;
 #
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
-#
+Select * from `city` where `countryCode` = "TJK" 
+or `countryCode` = "MRT"
+or `countryCode` = "AND"
+or `countryCode` = "PNG"
+or `countryCode` = "SJM";
 #
 # 14: Get the cities with a population between 200 and 700 inclusive
-#
+SELECT * from `city` where `population` between 200 and 700;
 #
 # 15: Get the countries with a population between 8000 and 20000 inclusive
-#
+SELECT * from `country` where `population` between 8000 and 20000;
 #
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
-#
+SELECT * from `country` where `indepyear` <0;
 #
 # 17: Get the countries that has no recorded independence year and a population above 1000000
-#
+Select * from `country` where `indepyear` is null and `population` >=1000000;
 #
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
-#
+SELECT * from `country` where `surfaceArea` <10 and `lifeExpectancy` is not null;
